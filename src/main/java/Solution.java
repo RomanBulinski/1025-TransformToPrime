21210d6a8a52f1693dd1fd090a2c97728c08db1f
bulir <aboandrus@gamil.com>
2021-08-28 15:44:54 +0200

    Implemented issue

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,6 +1,27 @@
+import java.util.Arrays;
+
 public class Solution {
-    public static int minimumNumber(int[] numbers)
-    {
-        return 0; // Do your magic!
+    public static int minimumNumber(int[] numbers) {
+
+        int result = 0;
+        boolean isNotPrime = true;
+        int sum = Arrays.stream(numbers).sum();
+
+        while (isNotPrime) {
+            if( checkIfPrimeNumber(sum++)){
+                isNotPrime = false;
+            }else{
+                result++;
+            }
+        }
+        return result;
+    }
+
+    private static boolean checkIfPrimeNumber(int number){
+        for(int i=2;i<number;i++) {
+            if(number%i==0)
+                return false;
+        }
+        return true;
     }
 }
